edf446e64a6da4f3b23f38fdcd042b1ce1eb4f86
Kruti Baraiya <baraiyakruti@gmail.com>
2021-04-01 21:52:29 +0530

    fixed flickering

--- dining-room.cpp
+++ dining-room.cpp
@@ -3,7 +3,6 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <glm/glm.hpp>
-#include <iostream>
 #include "Color.h"
 #include "Camera.h"
 
@@ -12,10 +11,46 @@
 GLfloat length = 2, thickness = 0.05;
 GLfloat Pos[] = {0,1,0,1};
 GLfloat Col[] = {0,1,0,1};
- Camera camera ({10, 10, 10}, {0, 0, 0});
- GLboolean isDragging = false;
- GLint dragX, dragY;
-void DrawObjects() {
+Camera camera ({10, 10, 10}, {0, 0, 0});
+GLboolean isDragging = false;
+GLint dragX, dragY;
+
+
+void DrawRoom() {
+    // Floor
+    glPushMatrix();
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, Colors::WHITE.rgba);
+    glTranslatef(0,-3,0);
+    glScalef(10,thickness,10);
+    glutSolidCube(length);
+    glPopMatrix();
+
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, Colors::RED.rgba);
+    // walls
+
+    glPushMatrix();
+    glTranslatef(0,7,-10);
+    glScalef(10,10,thickness);
+    glutSolidCube(length);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(-10,7,0);
+    glScalef(thickness,10,10);
+    glutSolidCube(length);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(10,7,0);
+    glScalef(thickness,10,10);
+    glutSolidCube(length);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(0,7,10);
+    glScalef(10,10,thickness);
+    glutSolidCube(length);
+    glPopMatrix();
 
     // teapot
     glPushMatrix();
@@ -31,9 +66,145 @@ void DrawObjects() {
     glutStrokeCharacter(GLUT_STROKE_MONO_ROMAN,'A');
     glPopMatrix();
 
-    glutSwapBuffers();
-}
-void DrawShelf() {
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, Colors::GREEN.rgba);
+    // Table top
+    glPushMatrix();
+    glScalef(2.0,thickness,1.5);
+    glutSolidCube(length);
+    glPopMatrix();
+
+    // Table Legs
+
+    static GLUquadric *gluQuadric = gluNewQuadric();
+    glPushMatrix();
+    glTranslatef(-length+thickness,0,3*length/4-thickness);
+    glRotatef(90,1,0,0);
+    gluCylinder(gluQuadric, thickness,thickness,3,50,50);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(length-thickness,0,3*length/4-thickness);
+    glRotatef(90,1,0,0);
+    gluCylinder(gluQuadric, thickness,thickness,3,50,50);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(-length+thickness,0,-3*length/4+thickness);
+    glRotatef(90,1,0,0);
+    gluCylinder(gluQuadric, thickness,thickness,3,50,50);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(length-thickness,0,-3*length/4+thickness);
+    glRotatef(90,1,0,0);
+    gluCylinder(gluQuadric, thickness,thickness,3,50,50);
+    glPopMatrix();
+
+
+    // Right chair bottom
+    glPushMatrix();
+    glTranslatef(length+1.5,-length/2,0);
+    glScalef(length/2,thickness,length/2);
+    glutSolidCube(length);
+    glPopMatrix();
+
+    // Right chair legs
+    glPushMatrix();
+    glTranslatef(length+1.5-length/2+thickness,-length/2,-length/2+thickness);
+    glRotatef(90,1,0,0);
+    gluCylinder(gluQuadric, thickness,thickness,length,50,50);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(length+1.5+length/2-thickness,-length/2,-length/2+thickness);
+    glRotatef(90,1,0,0);
+    gluCylinder(gluQuadric, thickness,thickness,length,50,50);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(length+1.5+length/2-thickness,-length/2,+length/2-thickness);
+    glRotatef(90,1,0,0);
+    gluCylinder(gluQuadric, thickness,thickness,length,25,25);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(length+1.5-length/2+thickness,-length/2,length/2-thickness);
+    glRotatef(90,1,0,0);
+    gluCylinder(gluQuadric, thickness,thickness,length,50,50);
+    glPopMatrix();
+
+    // Right chair back support rods
+
+    glPushMatrix();
+    GLfloat d = 0.27;
+    glRotatef(90,1,0,0);
+    glTranslatef(length+1.5+length/2-thickness,length/2-thickness,-length/2);
+    for(int i = 0; i < 8; i++) {
+        gluCylinder(gluQuadric,thickness,thickness,length,50,50);
+        glTranslatef(0,-d,0);
+    }
+    glPopMatrix();
+
+    // Right chair back support
+
+    glPushMatrix();
+    glTranslatef(length+1.5+length/2-thickness,length/2+thickness,0);
+    glScalef(thickness,0.1,length/2+thickness);
+    glutSolidCube(length);
+    glPopMatrix();
+
+
+    // Left chair bottom
+    glPushMatrix();
+    glTranslatef(-length-1.5,-length/2,0);
+    glScalef(length/2,thickness,length/2);
+    glutSolidCube(length);
+    glPopMatrix();
+
+    // Left chair legs
+    glPushMatrix();
+    glTranslatef(-length-1.5-length/2+thickness,-length/2,-length/2+thickness);
+    glRotatef(90,1,0,0);
+    gluCylinder(gluQuadric, thickness,thickness,length,50,50);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(-length-1.5+length/2-thickness,-length/2,-length/2+thickness);
+    glRotatef(90,1,0,0);
+    gluCylinder(gluQuadric, thickness,thickness,length,50,50);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(-length-1.5+length/2-thickness,-length/2,+length/2-thickness);
+    glRotatef(90,1,0,0);
+    gluCylinder(gluQuadric, thickness,thickness,length,25,25);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(-length-1.5-length/2+thickness,-length/2,length/2-thickness);
+    glRotatef(90,1,0,0);
+    gluCylinder(gluQuadric, thickness,thickness,length,50,50);
+    glPopMatrix();
+
+    // Left chair back support rods
+
+    glPushMatrix();
+    d = 0.27;
+    glRotatef(90,1,0,0);
+    glTranslatef(-length-1.5-length/2+thickness,length/2-thickness,-length/2);
+    for(int i = 0; i < 8; i++) {
+        gluCylinder(gluQuadric,thickness,thickness,length,50,50);
+        glTranslatef(0,-d,0);
+    }
+    glPopMatrix();
+
+    // Left chair back support
+
+    glPushMatrix();
+    glTranslatef(-length-1.5-length/2+thickness,length/2+thickness,0);
+    glScalef(thickness,0.1,length/2+thickness);
+    glutSolidCube(length);
+    glPopMatrix();
 
     // shelf backside
     glPushMatrix();
@@ -80,195 +251,8 @@ void DrawShelf() {
 
     glutSwapBuffers();
 }
-void DrawChair() {
-    static GLUquadric *gluQuadric = gluNewQuadric();
-    // Right chair bottom
-    glPushMatrix();
-    glTranslatef(length+1.5,-length/2,0);
-    glScalef(length/2,thickness,length/2);
-    glutSolidCube(length);
-    glPopMatrix();
 
-    // Right chair legs
-    glPushMatrix();
-    glTranslatef(length+1.5-length/2+thickness,-length/2,-length/2+thickness);
-    glRotatef(90,1,0,0);
-    gluCylinder(gluQuadric, thickness,thickness,length,50,50);
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(length+1.5+length/2-thickness,-length/2,-length/2+thickness);
-    glRotatef(90,1,0,0);
-    gluCylinder(gluQuadric, thickness,thickness,length,50,50);
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(length+1.5+length/2-thickness,-length/2,+length/2-thickness);
-    glRotatef(90,1,0,0);
-    gluCylinder(gluQuadric, thickness,thickness,length,25,25);
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(length+1.5-length/2+thickness,-length/2,length/2-thickness);
-    glRotatef(90,1,0,0);
-    gluCylinder(gluQuadric, thickness,thickness,length,50,50);
-    glPopMatrix();
-
-    // Right chair back support rods
-
-    glPushMatrix();
-    GLfloat d = 0.27;
-    glRotatef(90,1,0,0);
-    glTranslatef(length+1.5+length/2-thickness,length/2-thickness,-length/2);
-    for(int i = 0; i < 8; i++) {   
-        gluCylinder(gluQuadric,thickness,thickness,length,50,50);
-        glTranslatef(0,-d,0);
-    }
-    glPopMatrix();
-
-    // Right chair back support
-
-    glPushMatrix();
-    glTranslatef(length+1.5+length/2-thickness,length/2+thickness,0);
-    glScalef(thickness,0.1,length/2+thickness);
-    glutSolidCube(length);
-    glPopMatrix();
-    
-
-    // Left chair bottom
-    glPushMatrix();
-    glTranslatef(-length-1.5,-length/2,0);
-    glScalef(length/2,thickness,length/2);
-    glutSolidCube(length);
-    glPopMatrix();
-
-    // Left chair legs
-    glPushMatrix();
-    glTranslatef(-length-1.5-length/2+thickness,-length/2,-length/2+thickness);
-    glRotatef(90,1,0,0);
-    gluCylinder(gluQuadric, thickness,thickness,length,50,50);
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(-length-1.5+length/2-thickness,-length/2,-length/2+thickness);
-    glRotatef(90,1,0,0);
-    gluCylinder(gluQuadric, thickness,thickness,length,50,50);
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(-length-1.5+length/2-thickness,-length/2,+length/2-thickness);
-    glRotatef(90,1,0,0);
-    gluCylinder(gluQuadric, thickness,thickness,length,25,25);
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(-length-1.5-length/2+thickness,-length/2,length/2-thickness);
-    glRotatef(90,1,0,0);
-    gluCylinder(gluQuadric, thickness,thickness,length,50,50);
-    glPopMatrix();
-
-    // Left chair back support rods
-
-    glPushMatrix();
-    d = 0.27;
-    glRotatef(90,1,0,0);
-    glTranslatef(-length-1.5-length/2+thickness,length/2-thickness,-length/2);
-    for(int i = 0; i < 8; i++) {   
-        gluCylinder(gluQuadric,thickness,thickness,length,50,50);
-        glTranslatef(0,-d,0);
-    }
-    glPopMatrix();
-
-    // Left chair back support
-
-    glPushMatrix();
-    glTranslatef(-length-1.5-length/2+thickness,length/2+thickness,0);
-    glScalef(thickness,0.1,length/2+thickness);
-    glutSolidCube(length);
-    glPopMatrix();
-
-    glutSwapBuffers();
-}
-void DrawRoom() {
-    // Floor
-    glPushMatrix();
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, Colors::WHITE.rgba);
-    glTranslatef(0,-3,0);
-    glScalef(10,thickness,10);
-    glutSolidCube(length);
-    glPopMatrix();
-
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, Colors::RED.rgba);
-    // walls
-
-    glPushMatrix();
-    glTranslatef(0,7,-10);
-    glScalef(10,10,thickness);
-    glutSolidCube(length);
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(-10,7,0);
-    glScalef(thickness,10,10);
-    glutSolidCube(length);
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(10,7,0);
-    glScalef(thickness,10,10);
-    glutSolidCube(length);
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(0,7,10);
-    glScalef(10,10,thickness);
-    glutSolidCube(length);
-    glPopMatrix();
-
-    glutSwapBuffers();
-}
-void DrawTable() {
-
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, Colors::GREEN.rgba);
-    // Table top
-    glPushMatrix();
-    glScalef(2.0,thickness,1.5);
-    glutSolidCube(length);
-    glPopMatrix();
-
-    // Table Legs
-
-    static GLUquadric *gluQuadric = gluNewQuadric();
-    glPushMatrix();
-    glTranslatef(-length+thickness,0,3*length/4-thickness);
-    glRotatef(90,1,0,0);
-    gluCylinder(gluQuadric, thickness,thickness,3,50,50);
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(length-thickness,0,3*length/4-thickness);
-    glRotatef(90,1,0,0);
-    gluCylinder(gluQuadric, thickness,thickness,3,50,50);
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(-length+thickness,0,-3*length/4+thickness);
-    glRotatef(90,1,0,0);
-    gluCylinder(gluQuadric, thickness,thickness,3,50,50);
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(length-thickness,0,-3*length/4+thickness);
-    glRotatef(90,1,0,0);
-    gluCylinder(gluQuadric, thickness,thickness,3,50,50);
-    glPopMatrix();
-
-    
-
-
-    glutSwapBuffers(); 
-}
- void keyboard(unsigned char key, int x, int y) {
+void keyboard(unsigned char key, int x, int y) {
     switch (key) {
         case 'w':
             camera.translate(Camera::Direction::FORWARD);
@@ -295,10 +279,10 @@ void DrawTable() {
             break;
     }
     glutPostRedisplay();
- }
+}
 
 
- void special(int key, int x, int y) {
+void special(int key, int x, int y) {
     switch (key) {
         case GLUT_KEY_UP:
             camera.translate(Camera::Direction::UP);
@@ -310,9 +294,9 @@ void DrawTable() {
             break;
     }
     glutPostRedisplay();
- }
+}
 
- void mouseButton(int button, int state, int x, int y) {
+void mouseButton(int button, int state, int x, int y) {
     switch (button) {
         case GLUT_LEFT_BUTTON: {
             if (state == GLUT_DOWN) {
@@ -334,16 +318,16 @@ void DrawTable() {
             break;
     }
     glutPostRedisplay();
- }
+}
 
- void mouseMotion(int x, int y) {
+void mouseMotion(int x, int y) {
     if (isDragging) {
         camera.rotate(x - dragX, y - dragY);
         dragX = x;
         dragY = y;
         glutPostRedisplay();
     }
- }
+}
 void render() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glMatrixMode(GL_PROJECTION);
@@ -354,13 +338,10 @@ void render() {
     camera.lookAt();
     //gluLookAt(10,5,10,0,0,0,0,1,0);
 
-    DrawChair();
-    DrawTable();
-    DrawShelf();
     DrawRoom();
-    DrawObjects();
 
-    
+
+
 }
 
 int main(int argc, char **argv) {
@@ -375,16 +356,16 @@ int main(int argc, char **argv) {
     glLightfv(GL_LIGHT0, GL_DIFFUSE, Colors::GREEN.rgba);
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
-     glutDisplayFunc(render);
-     glutKeyboardFunc(keyboard);
-     glutSpecialFunc(special);
-     glutMouseFunc(mouseButton);
-     glutMotionFunc(mouseMotion);
-   
+    glutDisplayFunc(render);
+    glutKeyboardFunc(keyboard);
+    glutSpecialFunc(special);
+    glutMouseFunc(mouseButton);
+    glutMotionFunc(mouseMotion);
 
 
 
-    
+
+
     glutMainLoop();
 
     return 0;
